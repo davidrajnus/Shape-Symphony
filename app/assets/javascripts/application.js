46ad259f09c8456dd5657ed8397c7e17487ea965
DRAJ <davidrajnus@gmail.com>
2018-03-16 00:33:23 +0800

    added howler and paperjs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require howler
+//= require paper-full
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
